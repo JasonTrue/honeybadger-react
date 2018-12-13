9d3827674619c85fb3d3b1acd6ef451c7a532cad
Jason Truesdell <jason@yuzuten.com>
2018-12-13 16:45:19 +0900

    Updates

---
+++ precache-manifest.ed17b03283dc8639cc9a39193423492c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "470077f0d30f1075bfe8",
+    "url": "/honeybadger-react/static/css/main.9849cea3.chunk.css"
+  },
+  {
+    "revision": "470077f0d30f1075bfe8",
+    "url": "/honeybadger-react/static/js/main.470077f0.chunk.js"
+  },
+  {
+    "revision": "a093d8285b6c5009531d",
+    "url": "/honeybadger-react/static/js/1.a093d828.chunk.js"
+  },
+  {
+    "revision": "ad70b663a6b782eee7b4",
+    "url": "/honeybadger-react/static/js/runtime~main.ad70b663.js"
+  },
+  {
+    "revision": "e8ddf09e3795f48cf95d3e6c10eddcc9",
+    "url": "/honeybadger-react/index.html"
+  }
+];
